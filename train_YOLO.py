85ae6603204f54a85bcb2c2c11a411e4680a8cb2
s244820 <93318173+August-Boennelykke@users.noreply.github.com>
2026-03-04 08:30:28 +0100

    Image model files

---
+++ train_YOLO.py
@@ -0,0 +1,7 @@
+from ultralytics import YOLO
+
+# Load the smallest, fastest model (perfect for real-time EV3)
+model = YOLO('yolo11n.pt') 
+
+# Train for 50-100 rounds (epochs)
+model.train(data='path/to/data.yaml', epochs=100, imgsz=640)    
