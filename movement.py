822b3d627d7f83478ea675facbaa375562c818cf
s244820 <93318173+August-Boennelykke@users.noreply.github.com>
2026-03-04 08:50:44 +0100

    maybe now?

--- movement.py
+++ movement.py
@@ -1,13 +1,13 @@
 
 
 def turn_90_right():
-    return "right".encode("utf-8")
+    return "right\n".encode("utf-8")
 
 def turn_90_left():
-    return "left".encode("utf-8")
+    return "left\n".encode("utf-8")
 
 def drive_forward():
-    return "forward".encode("utf-8")
+    return "forward\n".encode("utf-8")
 
 def drive_backward():
-    return "backward".encode("utf-8")
+    return "backward\n".encode("utf-8")
