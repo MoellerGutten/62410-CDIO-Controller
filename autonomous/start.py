d2b677ce13ec80d154f6167e0784c2e80b078577
nordbobirk <36029360+nordbobirk@users.noreply.github.com>
2026-04-29 10:00:24 +0200

    merge

--- autonomous/start.py
+++ autonomous/start.py
@@ -1,7 +1,30 @@
-from model import state
 from stateManager import update_state
+from protocol import CommandName, Arguments, Instruction, InstructionType, Message, serialize_message
+from connection import connect
 
-def start_autonomous_session(state, frame, model, M, M_inv, logger):
+def start_autonomous_session(state,  logger):
     print("autonomous")
-    update_state(state, frame, model, M, M_inv,logger)
-    pass
+    update_state(state, logger)
+
+    socket = connect()
+
+    ball = state.balls[0]
+    while not state.robot.is_facing_point(ball.position, 5.0):
+        inst = Instruction(
+            name=CommandName.TURN_RIGHT,
+            type=InstructionType.COMMAND,
+            args=Arguments(),
+        )
+        msg = Message(instruction=inst)
+        s = serialize_message(msg)
+        socket.sendall(s.encode("utf-8"))
+
+    while not state.robot.distance_to_point(ball.position) > 5:
+        inst = Instruction(
+            name=CommandName.FORWARD,
+            type=InstructionType.COMMAND,
+            args=Arguments(),
+        )
+        msg = Message(instruction=inst)
+        s = serialize_message(msg)
+        socket.sendall(s.encode("utf-8"))
