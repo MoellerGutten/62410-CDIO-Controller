32f1c8ac2d00794ab544f6bc7d343cc74f57e2e8
nordbobirk <36029360+nordbobirk@users.noreply.github.com>
2026-06-10 11:24:24 +0200

    fix: write logs to stdout and log file

--- src/debug/log.py
+++ src/debug/log.py
@@ -1,20 +1,28 @@
-from logging import Logger, getLogger, DEBUG, Formatter, FileHandler
+from logging import Logger, getLogger, DEBUG, Formatter, FileHandler, StreamHandler
 from datetime import datetime
 from os import makedirs
+from sys import stdout
 
 _logger: Logger | None = None
 
-def _setup_logger() -> None:
+def _setup_logger() -> Logger:
     global _logger
 
     makedirs("logs", exist_ok=True)
     timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     path = f"logs/{timestamp}.log"
     logger = getLogger("controller")
-    logger.setLevel(DEBUG) # process all log levels including DEBUG
-    handler = FileHandler(path, encoding="utf-8")
-    handler.setFormatter(Formatter("[%(asctime)s] %(message)s\n"))
-    logger.addHandler(handler)
+    logger.setLevel(DEBUG)
+    formatter = Formatter("[%(asctime)s] %(message)s\n")
+
+    file_handler = FileHandler(path, encoding="utf-8")
+    file_handler.setFormatter(formatter)
+
+    stream_handler = StreamHandler(stdout)
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
     _logger = logger
     return logger
 
