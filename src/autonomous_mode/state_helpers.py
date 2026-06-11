d2118a61990856fcb03ae970178b553abc13162c
nordbobirk <36029360+nordbobirk@users.noreply.github.com>
2026-06-11 09:27:25 +0200

    fix typo ms->s

--- src/autonomous_mode/state_helpers.py
+++ src/autonomous_mode/state_helpers.py
@@ -34,7 +34,7 @@ def update_ball_count_estimate(state: ArenaState) -> int:
     estimated_ball_count = round(sum(ball_counts) / len(ball_counts))
     with state.lock:
         state.estimated_ball_count = estimated_ball_count
-    get_logger().debug(f"update_ball_count_estimate took {time() - begin}ms with {BALL_COUNT_ESTIMATION_SNAPSHOTS} snapshots")
+    get_logger().debug(f"update_ball_count_estimate took {time() - begin}s with {BALL_COUNT_ESTIMATION_SNAPSHOTS} snapshots")
     return estimated_ball_count
 
 
