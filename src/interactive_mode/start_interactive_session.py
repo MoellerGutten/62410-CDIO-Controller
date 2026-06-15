8f86cc696878e93036914e09935d3dd970ed77ce
nordbobirk <36029360+nordbobirk@users.noreply.github.com>
2026-06-15 09:52:19 +0200

    fix invalid command handling

--- src/interactive_mode/start_interactive_session.py
+++ src/interactive_mode/start_interactive_session.py
@@ -3,6 +3,7 @@ from src.interactive_mode.interactice_command_util import parse_input, build_mes
 from src.debug.log import get_logger
 
 def start_interactive_session() -> None:
+    logger = get_logger("start_interactive_session")
     connection = RobotConnection()
     while True:
         inp = input("Robot instruction > ").strip()
@@ -11,7 +12,10 @@ def start_interactive_session() -> None:
         if not inp:
             continue
         name, kwargs = parse_input(inp)
-        msg = build_message_from_short_command(name, kwargs)
-        response = connection.send_message(msg)
-        get_logger().info(f"Robot response: {response}")
-    get_logger().info("\nClosing connection.")
+        try:
+            msg = build_message_from_short_command(name, kwargs)
+            response = connection.send_message(msg)
+            logger.info(f"Robot response: {response}")
+        except:
+            logger.error("Invalid command")
+    logger.info("\nClosing connection.")
