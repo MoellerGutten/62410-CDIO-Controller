e37698de23f9d17c1781b5f6bf17d5f7384a354e
Bobber <bobberen@protonmail.com>
2026-06-16 14:52:12 +0200

    adjust turning

--- src/lib/algorithms.py
+++ src/lib/algorithms.py
@@ -4,11 +4,11 @@
 
 def turn_to_point_turn_ms(angle: float) -> int:
     """Determine how many milliseconds to turn for based on the angle to the point to which the robot is turning"""
-    return max(100, min(500, int(abs(angle) * 8)))
+    return max(100, min(1000, int(abs(angle) * 8)))
 
 def turn_to_point_turn_speed(angle: float) -> int:
     """Determine the speed with which to turn based on the angle to the point to which the robot is turning"""
-    return max(30, min(100, int(abs(angle) * 0.4)))
+    return max(30, min(100, int(abs(angle) * 0.5)))
 
 ############################
 # drive_forward algorithms #
