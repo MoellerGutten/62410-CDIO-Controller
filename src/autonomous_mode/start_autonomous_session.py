b9006d5eff7fe4b426e4a247c8298d1743105e0d
Tobias Møller Larsen <Tobias-alleroed@hotmail.com>
2026-06-17 10:09:50 +0200

    fix: typo

--- src/autonomous_mode/start_autonomous_session.py
+++ src/autonomous_mode/start_autonomous_session.py
@@ -24,7 +24,7 @@ def _select_next_ball(robot: Robot, balls_in_robot: int, state: ArenaState):
         next_ball = robot.get_nearest_non_vip_ball
     else:
         balls_left_before_delivery = BALLS_PER_DELIVERY - balls_in_robot if (state.estimated_ball_count + balls_in_robot) >= BALLS_PER_DELIVERY else state.estimated_ball_count
-        next_ball = robot.get_nearest_vip_ball if balls_left_before_delivery == 1 else next_ball = robot.get_nearest_non_vip_ball
+        next_ball = robot.get_nearest_vip_ball if balls_left_before_delivery == 1 else robot.get_nearest_non_vip_ball
 
     get_logger("_select_next_ball").debug(f"Next ball: {next_ball!r}, balls_in_robot: {balls_in_robot}, has_vip_balls: {vips_on_field}")
     return next_ball
