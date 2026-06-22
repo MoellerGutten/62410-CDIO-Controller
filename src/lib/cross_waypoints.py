702b9d35c108dff061315887d2bb5b755954bcd4
Bobber <bobberen@protonmail.com>
2026-06-22 08:32:36 +0200

    removed redundant casting

--- src/lib/cross_waypoints.py
+++ src/lib/cross_waypoints.py
@@ -1,9 +1,5 @@
-from math import sin, cos, radians
 from src.model.cross import Cross
-from pygame.math import Vector2
-from src.state.arena_config import ArenaConfig
-from src.lib.constants import CROSS_WAYPOINT_OFFSET
-import numpy as np
+from numpy import sin, cos, radians, array
 
 # look here to understand this math https://github.com/MoellerGutten/62410-CDIO-Controller/wiki/cross-approach-points
 
@@ -16,20 +12,15 @@ def get_cross_waypoints(cross: Cross) -> list[tuple[float, float]]:
     if cross is None:
         return []
 
-    p0 = np.array([cross.position[0], cross.position[1]])
-    heading = np.radians(cross.orientation)
-    h = np.array([np.cos(heading), np.sin(heading)])
-    h_hat = np.array([-h[1], h[0]])
+    p0 = array([cross.position[0], cross.position[1]])
+    heading = radians(cross.orientation)
+    h = array([cos(heading), sin(heading)])
+    h_hat = array([-h[1], h[0]])
     d = 20  # fix denne til at være en konstant og juster så den passer med ('kryds arm længde') + (robot bredde + buffer).
 
     wp1 = p0 + d * h + d * h_hat
     wp2 = p0 + d * h - d * h_hat
     wp3 = p0 - d * h - d * h_hat
     wp4 = p0 - d * h + d * h_hat
-
-    wp1 = tuple(wp1)
-    wp2 = tuple(wp2)
-    wp3 = tuple(wp3)
-    wp4 = tuple(wp4)
 
     return  [(float(wp[0]), float(wp[1])) for wp in [wp1, wp2, wp3, wp4]]
