13e722fe27a02c2c26e5770b3e5dd4efbec32beb
s244820 <93318173+August-Boennelykke@users.noreply.github.com>
2026-06-23 13:17:33 +0200

    delivery point down

--- src/lib/constants.py
+++ src/lib/constants.py
@@ -105,7 +105,7 @@ DRIVE_TO_CENTER_DISTANCE_TOLERANCE = 10 + ARUCO_OFFSET_X
 # drive_to_goal constants #
 ###########################
 
-GOAL_DELIVERY_POINT = (151.5, 121.5/2)
+GOAL_DELIVERY_POINT = (148, 121.5/2)
 """The point where the robot is considered at the goal for deliveries"""
 
 ###################################
