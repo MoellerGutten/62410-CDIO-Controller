d7c1619d33fd3806fd08f6dd7ac09c0032141872
Merian12 <justmerian50@gmail.com>
2026-06-24 10:32:15 +0200

    fix: kissing

--- src/autonomous_mode/collection_helpers.py
+++ src/autonomous_mode/collection_helpers.py
@@ -2,7 +2,7 @@ from math import hypot, cos, sin, radians
 
 from src.lib.cross_approach_points import get_cross_approach_points
 from src.autonomous_mode.movement_helpers import _start_ball_intake, drive_forward, escape_cross_zone, go_to, burst_into_ball, turn_to_heading, turn_to_point, burst_backward, handle_balls_in_radius
-from src.autonomous_mode.movement_helpers import burst_into_ball_slightly_smaller, _start_ball_intake, drive_forward, escape_cross_zone, go_to, burst_into_ball, turn_to_heading, turn_to_point, burst_backward, handle_balls_in_radius
+from src.autonomous_mode.movement_helpers import burst_into_ball_slightly_smaller, _start_ball_intake, drive_forward, escape_cross_zone, go_to, burst_into_ball, turn_to_heading, turn_to_point, burst_backward, handle_balls_in_radius,gentle_burst_edge_ball
 from src.model.arena_state import ArenaState
 from src.model.arena_edge import get_other_corner_edge
 from src.model.ball import Ball
@@ -12,7 +12,7 @@ from src.lib.connection import RobotConnection
 from src.lib.constants import CROSS_APPROACH_POINTS_HORIZONTAL_OFFSET, CROSS_APPROACH_POINTS_VERTICAL_OFFSET, CROSS_AVOIDANCE_WAYPOINTS_OFFSET, \
 CROSS_FINAL_APPROACH_HORIZONTAL_OFFSET, CROSS_FINAL_APPROACH_VERTICAL_OFFSET, CROSS_ZONE_MAX_CREEP_STEPS, CROSS_ZONE_VERIFY_RADIUS, \
 GO_TO_BALL_EDGE_APPROACH_RADIUS, GO_TO_BALL_APPROACH_RADIUS, EDGE_BALL_GENTLE_BURST_TARGET_RANGE
-from src.autonomous_mode.corners import advance_to_corner_ball, get_staging_data, back_towards_wall_and_turn, gentle_burst
+from src.autonomous_mode.corners import advance_to_corner_ball, get_staging_data, back_towards_wall_and_turn
 from src.autonomous_mode.state_helpers import await_robot, update_ball_count_estimate
 
 def collect_edge_ball(state: ArenaState, ball: Ball, connection: RobotConnection, staging_point: tuple[float, float]):
@@ -27,7 +27,7 @@ def collect_edge_ball(state: ArenaState, ball: Ball, connection: RobotConnection
     precise_mode=True,
 )
     _start_ball_intake(connection)
-    gentle_burst(state, connection, ball.position, EDGE_BALL_GENTLE_BURST_TARGET_RANGE, 20)
+    gentle_burst_edge_ball(state, connection, ball.position,EDGE_BALL_GENTLE_BURST_TARGET_RANGE)
     burst_backward(state, connection)
 
 def collect_normal_ball(state: ArenaState, ball: Ball, connection: RobotConnection):
