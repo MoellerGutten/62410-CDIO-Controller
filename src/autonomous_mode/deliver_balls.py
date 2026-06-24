efd6426499c7258d2145a046915f48ca26d211a9
s245299 <s245299@dtu.dk>
2026-06-24 10:29:25 +0200

    changed deliver_balls to increase precision

--- src/autonomous_mode/deliver_balls.py
+++ src/autonomous_mode/deliver_balls.py
@@ -1,9 +1,11 @@
-from src.autonomous_mode.movement_helpers import burst_backward, go_to, turn_to_point, _start_ejaculation, _stop_ball_intake
+from src.autonomous_mode.movement_helpers import burst_backward, go_to, turn_to_point, _start_ejaculation, \
+    _stop_ball_intake, creep_forward_step
 from src.autonomous_mode.state_helpers import await_robot
 from src.model.arena_state import ArenaState
 from src.lib.connection import RobotConnection
 from src.debug.log import get_logger
-from src.lib.constants import DRIVE_TO_CENTER_DISTANCE_TOLERANCE, GOAL_DELIVERY_POINT, ARENA_WIDTH_CM, ARENA_HEIGHT_CM
+from src.lib.constants import DRIVE_TO_CENTER_DISTANCE_TOLERANCE, GOAL_DELIVERY_POINT, ARENA_WIDTH_CM, ARENA_HEIGHT_CM, \
+    FINAL_GOAL_DELIVERY_POINT
 
 def deliver_balls(state: ArenaState, connection: RobotConnection) -> None:
     logger = get_logger("deliver_balls")    
@@ -12,6 +14,7 @@ def deliver_balls(state: ArenaState, connection: RobotConnection) -> None:
     _stop_ball_intake(connection)
     drive_to_center(state, connection)
     drive_to_goal(state, connection)
+    get_real_close(state, connection)
     _start_ejaculation(connection)
     burst_backward(state, connection)
 
@@ -47,3 +50,21 @@ def drive_to_goal(state: ArenaState, connection: RobotConnection):
 
     logger.debug("At goal\n")
 
+def get_real_close(state: ArenaState, connection: RobotConnection):
+    logger = get_logger("getting_real_close")
+    robot = await_robot(state, connection)
+
+    goal: tuple[float, float] = (ARENA_WIDTH_CM, ARENA_HEIGHT_CM / 2)
+
+    with state.lock:
+        state.target_point = goal
+
+    logger.debug("Commence get real close")
+
+    turn_to_point(state, connection, goal, precise_mode=True)
+    await_robot(state, connection)
+    while robot.position[0] < FINAL_GOAL_DELIVERY_POINT[0]:
+        creep_forward_step(state, connection)
+        robot = await_robot(state, connection)
+    turn_to_point(state, connection, goal, precise_mode=True)
+    logger.debug("At goal\n")
