2aafedafa01c3bdd7fe695a32e0212b981b10fe7
nordbobirk <36029360+nordbobirk@users.noreply.github.com>
2026-06-24 14:30:48 +0200

    increase waypoint zone inflation

--- src/lib/constants.py
+++ src/lib/constants.py
@@ -141,7 +141,7 @@ MAX_ROBOT_DETECTION_ATTEMPTS = 5
 CROSS_AVOIDANCE_WAYPOINTS_OFFSET = 12
 """The amount of padding for the bounding box around the cross. Used for setting waypoints in the corners of the bounding box"""
 
-CROSS_WAYPOINT_OFFSET = CROSS_AVOIDANCE_WAYPOINTS_OFFSET + 9.5
+CROSS_WAYPOINT_OFFSET = CROSS_AVOIDANCE_WAYPOINTS_OFFSET + 10
 """The distance from center of cross to the corner of the waypoint zone. Distance is the avoidance
     zone plus size of cross"""
 
@@ -219,7 +219,6 @@ EDGE_BALL_STAGING_POINT_OFFSET = 31
 #############################
 # corner handling constants #
 #############################
-
 
 CORNER_THRESHOLD = 24
 """If a ball is within this distance of a corner measured along the wall that the ball is adjacent to, it is considered a corner ball"""
