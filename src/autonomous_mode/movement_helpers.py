b724c6cc67aa30bab7c2f99e5bc35d3d3820df19
Bobber <bobberen@protonmail.com>
2026-06-24 16:05:07 +0200

    update waypoints after escape

--- src/autonomous_mode/movement_helpers.py
+++ src/autonomous_mode/movement_helpers.py
@@ -288,6 +288,7 @@ def go_to(state: ArenaState
             # dumb fucking hack: set a target point far away for max speed
             drive_forward(state, connection, (1000, 1000))
             await_robot(state, connection)
+        waypoints = calculate_shortest_waypoint_path(state, point) if state.cross is not None else []
 
     waypoints.append(point)
 
