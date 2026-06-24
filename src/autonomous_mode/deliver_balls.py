a5c8a316efc1939fc6d8d88ddf0c8771049a7b47
nordbobirk <36029360+nordbobirk@users.noreply.github.com>
2026-06-24 13:05:38 +0200

    tidy up

--- src/autonomous_mode/deliver_balls.py
+++ src/autonomous_mode/deliver_balls.py
@@ -1,22 +1,20 @@
-from src.autonomous_mode.movement_helpers import burst_backward, go_to, turn_to_point, _start_ejaculation,\
-    _stop_ball_intake, creep_forward_step
+from src.autonomous_mode.movement_helpers import burst_backward, go_to, turn_to_point, _start_ejaculation, _stop_ball_intake, creep_forward_step
 from src.autonomous_mode.state_helpers import await_robot
 from src.model.arena_state import ArenaState
 from src.lib.connection import RobotConnection
 from src.debug.log import get_logger
-from src.lib.constants import DRIVE_TO_CENTER_DISTANCE_TOLERANCE, GOAL_DELIVERY_POINT, ARENA_WIDTH_CM, ARENA_HEIGHT_CM, \
-    FINAL_GOAL_DELIVERY_POINT, GET_REAL_CLOSE_MAX_ITER
-
-from src.lib.constants import DRIVE_TO_CENTER_DISTANCE_TOLERANCE, GOAL_DELIVERY_POINT, ARENA_WIDTH_CM, ARENA_HEIGHT_CM, BALLS_PER_DELIVERY
+from src.lib.constants import GOAL_DELIVERY_POINT, ARENA_WIDTH_CM, ARENA_HEIGHT_CM, FINAL_GOAL_DELIVERY_POINT, GET_REAL_CLOSE_MAX_ITER
 from time import time
+
+
 def deliver_balls(state: ArenaState, connection: RobotConnection) -> None:
     logger = get_logger("deliver_balls")    
     logger.debug("Commence delivery")
 
     _stop_ball_intake(connection)
-    drive_to_center(state, connection)
-    drive_to_goal(state, connection)
-    get_real_close(state, connection)
+    _drive_to_center(state, connection)
+    _drive_to_goal(state, connection)
+    _get_real_close(state, connection)
     _start_ejaculation(connection)
 
     if state.is_final_delivery:
@@ -30,7 +28,7 @@ def deliver_balls(state: ArenaState, connection: RobotConnection) -> None:
     logger.debug("Delivery done\n")
 
 
-def drive_to_center(state: ArenaState, connection: RobotConnection):
+def _drive_to_center(state: ArenaState, connection: RobotConnection):
     logger = get_logger("drive_to_center")
 
     robot = await_robot(state, connection)
@@ -46,7 +44,7 @@ def drive_to_center(state: ArenaState, connection: RobotConnection):
     logger.debug("Turned to goal")
 
         
-def drive_to_goal(state: ArenaState, connection: RobotConnection):
+def _drive_to_goal(state: ArenaState, connection: RobotConnection):
     logger = get_logger("drive_to_goal")
 
     goal: tuple[float, float] = (ARENA_WIDTH_CM, ARENA_HEIGHT_CM / 2)
@@ -62,7 +60,7 @@ def drive_to_goal(state: ArenaState, connection: RobotConnection):
 
     logger.debug("At goal\n")
 
-def get_real_close(state: ArenaState, connection: RobotConnection):
+def _get_real_close(state: ArenaState, connection: RobotConnection):
     logger = get_logger("getting_real_close")
     robot = await_robot(state, connection)
 
